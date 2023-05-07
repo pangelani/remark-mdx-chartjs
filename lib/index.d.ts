bbd1df1eae52b2234c36a1ad566fef7f16bab74b
Pablo <pablo.angelani@gmail.com>
2023-05-07 18:30:36 -0300

    Add declaration files

---
+++ lib/index.d.ts
@@ -0,0 +1,16 @@
+import { Plugin } from 'unified';
+import { Root } from 'mdast';
+
+interface RemarkMdxChartJSOptions {
+    chartJs: object;
+    imports: Record<string, boolean>;
+}
+/**
+ * Returns the transformer which acts on the MDAST tree and given VFile.
+ *
+ * @param {object} options
+ * @return {function}
+ */
+declare const remarkMdxChartJS: Plugin<[Partial<RemarkMdxChartJSOptions>?], Root>;
+
+export { RemarkMdxChartJSOptions, remarkMdxChartJS as default };
