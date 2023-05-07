71de5522337eaab3d62feaf0a408ec307e09bf56
Pablo Angelani <pangelani@users.noreply.github.com>
2023-05-07 19:56:58 -0300

    Update index.d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,7 +2,7 @@ import { Plugin } from 'unified';
 import { Root } from 'mdast';
 
 interface RemarkMdxChartJSOptions {
-    chartJs: object;
+    chartJs: any;
     imports: Record<string, boolean>;
 }
 /**
